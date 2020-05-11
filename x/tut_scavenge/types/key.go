9d34806c4d2b5b9ae33abc06c71b5f04a7aebe7e
akigugale <gugaleaki@gmail.com>
2020-05-12 00:12:50 +0530

    intial commit

---
+++ x/tut_scavenge/types/key.go
@@ -0,0 +1,19 @@
+package types
+
+const (
+	// ModuleName is the name of the module
+	ModuleName = "tut_scavenge"
+
+	// StoreKey to be used when creating the KVStore
+	StoreKey = ModuleName
+
+  // RouterKey to be used for routing msgs
+  RouterKey = ModuleName
+
+  // QuerierRoute to be used for querierer msgs
+	QuerierRoute = ModuleName
+
+
+	ScavengePrefix = "sk-"
+	CommitPrefix   = "ck-"
+)
